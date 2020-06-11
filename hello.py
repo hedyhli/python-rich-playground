2e2ad9df13f7a63e3678c3429c9651ddca014bec
Hedy Li <hedyhyry@gmail.com>
2020-06-11 15:55:13 +0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+from rich import Console
+
+console = Console()
+
+console.print('[magenta]Hello [blue]World')
---
+++ styles.py
@@ -0,0 +1,5 @@
+from rich.console import Console
+
+console = Console()
+
+console.print("Hello", style="rgb(175,0,255)")
