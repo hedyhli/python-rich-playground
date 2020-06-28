37831f3f3aaf651673f66ab701ac767f4553ef6a
Hedy Li <hedyhyry@gmail.com>
2020-06-28 11:16:01 +0800

    add columns.py

---
+++ columns.py
@@ -0,0 +1,23 @@
+"""
+This example shows how to display content in columns.
+The data is pulled from https://randomuser.me
+"""
+
+import json
+from urllib.request import urlopen
+
+from rich import print
+from rich.columns import Columns
+from rich.panel import Panel
+
+
+def get_content(user):
+    """Extract text from user dict."""
+    country = user["location"]["country"]
+    name = f"{user['name']['first']} {user['name']['last']}"
+    return f"[b]{name}[/b]\n[yellow]{country}"
+
+
+users = json.loads(urlopen("https://randomuser.me/api/?results=30").read())["results"]
+user_renderables = [Panel(get_content(user), expand=True) for user in users]
+print(Columns(user_renderables))
